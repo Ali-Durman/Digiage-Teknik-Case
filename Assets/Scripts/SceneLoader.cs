bacc7e7d170a31920989cd5510d93af79e445ba3
jiyan kumek <jiyankumek@gmail.com>
2022-12-31 21:11:11 +0300

    LoadScene

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private PLayerManager playermanager;
     [SerializeField] private GameObject _loseCanvas;
     [SerializeField] private GameObject _winCanvas;
+
     public void LoadScene()
     {
         SceneManager.LoadScene("Level1");
@@ -16,10 +19,18 @@ public class SceneLoader : MonoBehaviour
 
     private void Start()
     {
+        playermanager = GetComponent<PLayerManager>();
+        
         _winCanvas.SetActive(false);
         _loseCanvas.SetActive(false);
     }
 
-
+    private void Update()
+    {
+        if (playermanager._numberOfStickmans == 0 )
+        {
+            _loseCanvas.SetActive(true);
+        }
+    }
 
 }
